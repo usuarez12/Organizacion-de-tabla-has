e40fcbd4acf11897f3b2f8a72a4643e72feda019
uriel <urielgonzalez89pl@gmail.com>
2025-11-20 23:42:05 -0600

    Codigo final

--- TablaHash.java
+++ TablaHash.java
@@ -49,4 +49,54 @@ public class TablaHash {
         }
         return tabla;
     }
+
+     public static int[] leerArchivo(String nombre) throws IOException {
+
+        List<Integer> lista = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new FileReader(nombre));
+        String linea;
+
+        while ((linea = br.readLine()) != null) {
+            linea = linea.trim();
+            if (linea.isEmpty()) continue;
+
+            String[] partes = linea.split("\\s+");
+
+            for (String p : partes) {
+                lista.add(Integer.parseInt(p));
+            }
+        }
+
+        br.close();
+
+        int[] arr = new int[lista.size()];
+        for (int i = 0; i < lista.size(); i++)
+            arr[i] = lista.get(i);
+
+        return arr;
+    }
+
+    public static void escribirTabla(String archivo, List<List<Integer>> tabla) throws IOException {
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(archivo));
+
+        for (int i = 0; i < tabla.size(); i++) {
+            bw.write(i + ": ");   
+            for (int num : tabla.get(i)) {
+                bw.write(num + " ");
+            }
+
+            bw.newLine();
+        }
+
+        bw.close();
+    }
+    
+    public static void escribirTexto(String archivo, String texto) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(archivo));
+        bw.write(texto);
+        bw.newLine();
+        bw.close();
+    }
 }
